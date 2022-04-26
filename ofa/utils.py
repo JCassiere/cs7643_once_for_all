43e6d970828a91e8be0ecf1f8248f542dddbdf87
anbutchers <74564989+team-105@users.noreply.github.com>
2022-04-26 01:40:22 -0400

    updated progressive_shrinking_test_MNIST.py and added code for plots

--- ofa/utils.py
+++ ofa/utils.py
@@ -1,5 +1,7 @@
 import torch
-
+import matplotlib.pyplot as plt
+import json
+import numpy as np
 
 def get_device():
     return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -7,3 +9,21 @@ def get_device():
 def get_num_params(net):
     param_count = sum([torch.numel(p) for p in net.parameters()])
     return param_count
+    
+def plot_curves(train_accuracy, test_accuracy):
+    fig, ax = plt.subplots()
+    train = ax.plot(np.arange(0.0, len(train_accuracy)),\
+                         train_accuracy, label='Train Accuracy')
+    test = ax.plot(np.arange(0.0, len(test_accuracy)),\
+                         test_accuracy, label='Test Accuracy')
+    ax.set(title='Accuracy Curve', xlabel='Epochs', ylabel='Accuracy')
+    y_start, y_end = ax.get_ylim()
+    ax.legend(loc='upper right')
+    plt.xticks(np.arange(0.0, len(train_accuracy), 5))
+    plt.yticks(np.arange(0.0, y_end, 0.1))
+    plt.show()
+    fig.savefig("Plot.png")
+
+f = open('../experiments/cifar10_100_epochs_per_stage_1650866734/big_network_results.json')
+data = json.load(f)
+plot_curves(data['train_accuracies'],data['val_accuracies']['K5-D4-ExR6'])
--- progressive_shrinking_test_MNIST.py
+++ progressive_shrinking_test_MNIST.py
@@ -1,85 +1,17 @@
+import time
 import torch.nn
-import torchvision
-from torch.utils.data import DataLoader
-import torch.utils.data as data_utils
 from ofa.mobilenetv3 import mobilenetv3_large, mobilenetv3_small
-from ofa.mobilenetv3_ofa import mobilenetv3_ofa
-from ofa.progressive_shrinking import progressive_shrinking_from_scratch
-from ofa.progressive_shrinking import train_loop
-import cProfile
-
-def get_num_params(optimizer):
-    params = optimizer.param_groups[0]['params']
-    param_count = sum([torch.numel(p) for p in params])
-    return param_count
-    
-def large_test_ofa_net():
-    output_widths = [16, 16, 24, 40, 80, 112, 160, 960, 1280]
-    use_squeeze_excites = [False, False, True, False, True, True, True, True, True]
-    use_hard_swishes = [True, False, False, True, True, True, True, True, True]
-    strides = [1, 1, 1, 2, 1, 1, 1, 1, 1]
-    net = mobilenetv3_ofa(num_classes=100, output_widths=output_widths, use_squeeze_excites=use_squeeze_excites,
-                          use_hard_swishes=use_hard_swishes, strides=strides)
-    return net
-
-def get_device():
-    return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
-def small_test_ofa_net():
-    output_widths = [16, 16, 24, 64, 118, 800, 960]
-    use_squeeze_excites = [False, False, True, False, True, True, True]
-    use_hard_swishes = [True, False, False, True, True, True, True]
-    strides = [1, 1, 2, 2, 2, 1, 1]
-
-    net = mobilenetv3_ofa(num_classes=100, output_widths=output_widths, use_squeeze_excites=use_squeeze_excites,
-                          use_hard_swishes=use_hard_swishes, strides=strides, width_mult=0.25)
-    return net
-    
-def get_MNIST_dataloaders(device, subset=False):
-    def collate_fn_to_device(batch):
-        # adapted from pytorch default_collate_fn
-        transposed = list(zip(*batch))
-        images = torch.stack(transposed[0], 0).to(device)
-        targets = torch.tensor(transposed[1]).to(device)
-        
-        return images, targets
-    
-    MNIST_train = torchvision.datasets.MNIST(
-        root='./data/MNIST',
-        train=True,
-        download=True,
-        transform=torchvision.transforms.Compose([
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
-            torchvision.transforms.Normalize((0.1307,), (0.3081,)),
-        ]))
-    if subset:
-        indices = torch.arange(6000)
-        MNIST_train = data_utils.Subset(MNIST_train, indices)
-    
-    MNIST_test = torchvision.datasets.MNIST(
-        root='./data/MNIST',
-        train=False,
-        download=True,
-        transform=torchvision.transforms.Compose([
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
-            torchvision.transforms.Normalize((0.1307,), (0.3081,)),
-        ]))
-    if subset:
-        indices = torch.arange(1000)
-        MNIST_test = data_utils.Subset(MNIST_test, indices)
-    train_data_loader = DataLoader(MNIST_train, batch_size=128, shuffle=True,
-                                   collate_fn=collate_fn_to_device)
-    test_data_loader = DataLoader(MNIST_test, batch_size=128, shuffle=True,
-                                  collate_fn=collate_fn_to_device)
-    return train_data_loader, test_data_loader
+from ofa.progressive_shrinking import progressive_shrinking_from_scratch, train_big_network, train_elastic_kernel,\
+    train_elastic_depth_stage_1, train_elastic_depth_stage_2
+from ofa.utils import get_device
+from ofa.experiment import Experiment
+from ofa.datasets import get_dataloaders
     
 def train_mobilenetv3_MNIST():
     device = get_device()
-    train_data_loader, test_data_loader = get_MNIST_dataloaders(device)
+    train_data_loader, test_data_loader = get_dataloaders(device)
     criterion = torch.nn.CrossEntropyLoss()
-    net = mobilenetv3_small(num_classes=100)
+    net = mobilenetv3_small(num_classes=10)
     net.to(device)
     optimizer = torch.optim.SGD(net.parameters(), lr=0.1, weight_decay=1e-5, momentum=0.9)
     
@@ -105,68 +37,51 @@ def train_mobilenetv3_MNIST():
         print("Epoch {} accuracy: {}".format(epoch, accuracy))
 
 def train_mobilenetv3ofa_MNIST_no_shrinking():
-    device = get_device()
-    train_data_loader, test_data_loader = get_MNIST_dataloaders(device)
-
-    net = small_test_ofa_net()
-    net.to(device)
-    
-    train_loop(net, train_data_loader, test_data_loader, lr=0.1, epochs=100,
-               depth_choices=[4], kernel_choices=[7], expansion_ratio_choices=[6])
-
-
-def learn_on_small_kernel_only():
-    device = get_device()
-    train_data_loader, test_data_loader = get_MNIST_dataloaders(device, subset=True)
-    
-    net = small_test_ofa_net()
-    net.to(device)
-    
-    train_loop(net, train_data_loader, test_data_loader, lr=0.1, epochs=100,
-               depth_choices=[4], kernel_choices=[3], expansion_ratio_choices=[6])
+    exp_kwargs = {
+        "dataset_name": "mnist",
+        "experiment_name": "big_net_only_MNIST_{}".format(int(time.time()))
+    }
+    experiment = Experiment(**exp_kwargs)
+    train_big_network(experiment)
     
         
 def test_elastic_kernel():
-    device = get_device()
-    train_data_loader, test_data_loader = get_MNIST_dataloaders(device)
+    exp_kwargs = {
+        "dataset_name": "mnist",
+        "experiment_name": "my_experiment"
+    }
+    experiment = Experiment(**exp_kwargs)
+    # Note that this will only work if you have previously run an experiment called "my_experiment"
+    # and the big_network was trained
+    train_elastic_kernel(experiment, load_stage="big_network")
 
-    net = small_test_ofa_net()
-    net.to(device)
-    train_loop(net, train_data_loader, test_data_loader, lr=0.64, epochs=10,
-               depth_choices=[4], kernel_choices=[7, 5, 3],
-               expansion_ratio_choices=[6])
+def test_elastic_depth():
+    exp_kwargs = {
+        "dataset_name": "mnist",
+        "experiment_name": "my_experiment"
+    }
+    experiment = Experiment(**exp_kwargs)
+    # Note that this will only work if you have previously run an experiment called "my_experiment"
+    # and the big_network was trained
+    train_elastic_depth_stage_1(experiment, load_stage="big_network")
+    train_elastic_depth_stage_2(experiment)
 
 def test_progressive_shrinking():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    train_data_loader, test_data_loader = get_MNIST_dataloaders(device)
+    exp_kwargs = {
+        "dataset_name": "mnist",
+        "experiment_name": "mnist_100_epochs_per_stage_{}".format(int(time.time())),
+        "base_net_epochs": 100,
+        "elastic_kernel_epochs": 100,
+        "elastic_depth_epochs_stage_1": 25,
+        "elastic_depth_epochs_stage_2": 100,
+        "elastic_width_epochs_stage_1": 25,
+        "elastic_width_epochs_stage_2": 100
+    }
+    experiment = Experiment(**exp_kwargs)
+    progressive_shrinking_from_scratch(experiment)
 
-    net = small_test_ofa_net()
-    # net = large_test_ofa_net()
-    net.to(device)
-    progressive_shrinking_from_scratch(train_data_loader, test_data_loader, net, base_net_epochs=50,
-                                       elastic_kernel_epochs=100, elastic_depth_epochs_stage_1=5,
-                                       elastic_depth_epochs_stage_2=50, elastic_width_epochs_stage_1=5,
-                                       elastic_width_epochs_stage_2=50, base_net_lr=0.24, elastic_kernel_lr=0.20,
-                                       elastic_depth_lr_stage_1=0.05, elastic_depth_lr_stage_2=0.1,
-                                       elastic_width_lr_stage_1=0.05, elastic_width_lr_stage_2=0.1, )
-    # progressive_shrinking(train_data_loader, test_data_loader, net, base_net_epochs=25,
-    #                       elastic_kernel_epochs=25, elastic_depth_epochs_stage_1=5,
-    #                       elastic_depth_epochs_stage_2=25, elastic_width_epochs_stage_1=5,
-    #                       elastic_width_epochs_stage_2=25, base_net_lr=0.24, elastic_kernel_lr=0.20,
-    #                       elastic_depth_lr_stage_1=0.05, elastic_depth_lr_stage_2=0.1,
-    #                       elastic_width_lr_stage_1=0.05, elastic_width_lr_stage_2=0.1,)
-    # progressive_shrinking(train_data_loader, test_data_loader, net, base_net_lr=0.24, elastic_kernel_lr=0.20,
-    #                       elastic_depth_lr_stage_1=0.08, elastic_depth_lr_stage_2=0.16,
-    #                       elastic_width_lr_stage_1=0.08, elastic_width_lr_stage_2=0.16)
 
-# TODO - try learning just with elastic depth and/or elastic width
-#  I think there is a bug with the elastic kernel
 if __name__ == "__main__":
-    # train_mobilenetv3_MNIST()
-    # cProfile.run('train_mobilenetv3ofa_MNIST()', 'profile')
-    # train_mobilenetv3ofa_MNIST()
-    # learn_on_small_kernel_only()
-    
     # test_elastic_kernel()
-    # 1 epoch/30s on colab
+    # test_elastic_depth()
     test_progressive_shrinking()
